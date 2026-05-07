3db66068798d4c84f1825ae54f8f45cc15536c7b
topmac217 <marcotomei@tiscali.it>
2026-05-07 15:44:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+  // Questo codice serve solo a far capire ad Android che è una vera app
+  event.respondWith(fetch(event.request));
+});
